901e876deac00c54306bac91da58e347ac141d89
Suhaas-code <borra.219311240@muj.manipal.edu>
2025-04-22 17:59:10 +0530

    font-changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
 module.exports = {
-  purge: ['./src/**/*.{html,js}', './public/index.html'],
+  // purge: ['./src/**/*.{html,js}', './public/index.html'],
+  content: ["./**/*.html"], // include your HTML files,
+  safelist: ['bg-black'],
   darkMode: 'class', // Enable dark mode using the 'class' strategy
   theme: {
+    fontFamily: {
+      sans: ['"Open Sans"', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
     extend: {
       colors: {
         primary: '#4F46E5',
         secondary: '#3B82F6',
-        darkBackground: '#1a1a1a', // Add a darker background color
+        darkBackground: '#1a1a1a',
       },
       spacing: {
         '128': '32rem',
@@ -14,8 +20,6 @@ module.exports = {
       },
     },
   },
-  variants: {
-    extend: {},
-  },
+  darkMode: 'class',
   plugins: [],
 }
